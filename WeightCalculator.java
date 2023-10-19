956cd45d859b209fbb5f683816f99fb1b5e026bc
Aran Saseelan <aran.saseelan@gmail.com>
2023-10-19 19:33:42 -0400

    Second commit

--- WeightCalculator.java
+++ WeightCalculator.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class WeightCalculator{
     public static void main(String[] args){ //Main Class
         Scanner sc = new Scanner(System.in); 
-        System.out.println("How much weight are you in pounds?");
-        double weight_pounds = sc.nextInt(); 
+        double weight_pounds = -1;
+        double height_meters = -1;
+        while (!(weight_pounds > 0 && height_meters > 0 )){//Keeps on asking for user input until user puts a weight which is greater then zero
+            if (weight_pounds < 0 ){ 
+                System.out.println("How much weight are you in pounds?");
+                weight_pounds = sc.nextDouble(); 
+            }
+            if (height_meters < 0 ){
+                System.out.println("What is your height in meters? ");
+                height_meters = sc.nextDouble();
+            }
+        }
         double weight_kgs = weight_To_Pounds(weight_pounds);
         Person Aran = new Person("Aran", 19, "Male", weight_kgs); //Objects - Objects access the constructor
         System.out.printf("Your weight in kgs is %.2f %s", Aran.weight , "kgs");
@@ -29,4 +39,9 @@ class Person{
         this.gender = gender; 
         this.weight = weight; 
     }
+
+    public static double bmi_calculated(double weight_kgs, double height_meters){
+        return weight_kgs / (height_meters * height_meters);
+    }
+
 }
