a4a6a5e69184a6cbf499a563f218c2e07f920dfd
Aran Saseelan <aran.saseelan@gmail.com>
2023-11-02 09:42:28 -0400

    Made the code more readable and cleaned up the landing page UI.

--- LandingPage.java
+++ LandingPage.java
@@ -1,32 +1,95 @@
-import javax.swing.*; // Import Java Swing library for creating GUI
-import java.awt.*; // Import Java AWT for GUI components
-import java.awt.event.ActionEvent; // Import Java AWT event for GUI components
-import java.awt.event.ActionListener; // Import Java AWT event listener for GUI components
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.ActionEvent; 
+import java.awt.event.ActionListener; 
 
 public class LandingPage {
-    private JPanel landingPage; //Private keeps the variables encapsulated and hidden from other classes making it less complex
+    private JPanel landingPage;
     private JLabel welcomeLabel;
     private JButton proceedButton;
     private JFrame frame;
 
     public LandingPage(JFrame frame) {
+        
+
         this.frame = frame;
         landingPage = new JPanel();
-        landingPage.setLayout(new FlowLayout(FlowLayout.LEFT));
-        landingPage.setBackground(Color.pink); // Set the background color to blue
+        landingPage.setBackground(Color.PINK); 
+
+        // Set layout for the panel
+        GridBagLayout layout = new GridBagLayout();
+        landingPage.setLayout(layout);
+
+        // Create constraints for the layout
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        // Load the image and create a label with the image
+        ImageIcon imageIcon = new ImageIcon("C:\\Users\\Aran Saseelan\\Desktop\\VsCode Projects\\WeightCalculator\\Figma Icons\\iphone.png"); 
+        Image image = imageIcon.getImage();
+        Image scaledImage = image.getScaledInstance(400, 500, Image.SCALE_SMOOTH);
+        imageIcon = new ImageIcon(scaledImage);
+        JLabel imageLabel = new JLabel(imageIcon); 
+
+        // Load the second image and create a label with the image
+        ImageIcon imageIcon2 = new ImageIcon("C:\\Users\\Aran Saseelan\\Desktop\\VsCode Projects\\WeightCalculator\\Figma Icons\\StartToday.jpg"); 
+        Image image2 = imageIcon2.getImage();
+        Image scaledImage2 = image2.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        imageIcon2 = new ImageIcon(scaledImage2);
+        JLabel imageLabel2 = new JLabel(imageIcon2);
+
+        // Create and customize welcomeLabel
         welcomeLabel = new JLabel("WeightLossHelper");
-        landingPage.add(welcomeLabel);
+        welcomeLabel.setFont(new Font("Monospaced", Font.BOLD, 30)); 
+        welcomeLabel.setForeground(Color.WHITE); 
+
+        // Create and customize goalsLabel
+        JLabel goalsLabel = new JLabel("Reach your goals");
+        goalsLabel.setFont(new Font("Monospaced", Font.BOLD, 70)); 
+        goalsLabel.setForeground(Color.WHITE); 
+
+        // Create and customize withWeightLossHelper
+        JLabel withWeightLossHelper = new JLabel("with WeightLossHelper");
+        withWeightLossHelper.setFont(new Font("Monospaced", Font.BOLD, 40)); 
+        withWeightLossHelper.setForeground(Color.WHITE); 
+
+        // Set constraints for the layout
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.insets = new Insets(0, 50, 0, 0); 
+        gbc.weightx = 1; 
+        gbc.weighty = 1; 
+        gbc.gridx = 0; 
+        gbc.gridy = 0; 
+
+        // Add components to the panel with the constraints
+        landingPage.add(welcomeLabel, gbc); 
+
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.insets = new Insets(0, 0, 0, 10);
+        landingPage.add(imageLabel, gbc); 
+
+        gbc.anchor = GridBagConstraints.CENTER;
+        landingPage.add(imageLabel2, gbc); 
+        gbc.insets = new Insets(200, 0, 0, 200);
+
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(0, 20, 200, 0); 
+        landingPage.add(goalsLabel, gbc); 
+        
+        gbc.insets = new Insets(0, 20, 85, 0); 
+        landingPage.add(withWeightLossHelper, gbc); 
+        
+        gbc.insets = new Insets(100, 0, 0, 0); 
         proceedButton = new JButton("Proceed");
-        landingPage.add(proceedButton, BorderLayout.SOUTH);
+        landingPage.add(proceedButton, gbc);
 
         // Add action listener to proceed button
         proceedButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) { //If the button is clicked, the following code will be executed
+            public void actionPerformed(ActionEvent e) { 
                 // Remove landing page and add weight calculator panel
                 frame.getContentPane().removeAll();
-                frame.revalidate(); // Refresh the frame
-                frame.repaint(); // Repaint the frame
+                frame.revalidate(); 
+                frame.repaint(); 
             }
         });
     }
